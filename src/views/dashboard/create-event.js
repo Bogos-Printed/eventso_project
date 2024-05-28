ca9e7c548e63dfdcd3d752d93f8969dd2d47c6ff
Bogos-Printed <beauclair.nat@gmail.com>
2024-05-28 16:57:15 +0200

    feat(create event): add creation event

---
+++ src/views/dashboard/create-event.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const createEvent = async (formData) => {
+  const {
+    image,
+    title,
+    description,
+    category,
+    location,
+    date
+  } = formData;
+  console.log(formData);
+
+  const url = 'http://127.0.0.1:80/event/add';
+  try {
+    const response = await axios.post(url, {
+      image,
+      title,
+      description,
+      location,
+      date,
+      category
+    }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default createEvent;
