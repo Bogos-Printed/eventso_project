1cbb0ec89f22e15643dbe7f0cd0974331a2e55c4
Bogos-Printed <beauclair.nat@gmail.com>
2024-06-14 11:56:17 +0200

    feat(nav): :sparkles: add user dropdown & icon script

--- src/views/nav.js
+++ src/views/nav.js
@@ -7,9 +7,19 @@ export default () => (`
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="/dashboard">Dashboard</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" id='logIn-button'>
           <a class="nav-link" href="/log-in">Log in</a>
         </li>
+        <div class="dropdown d-none" id='user-menu'>
+          <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="fa-solid fa-user-large"></i>
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/dashboard">Dashboard</a></li>
+            <li><a class="dropdown-item" href="/settings">Settings</a></li>
+            <li><a class="dropdown-item" href="/" id="disconnect">Disconnect</a></li>
+          </ul>
+        </div>
       </ul>
 
     </div>
